80f04c9fe828db5a141f7d58c53d1b25b05276f3
Dhanabal <vel.dhanabal@gmail.com>
2016-08-21 17:00:54 +0530

    added domain to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :host                 => "success-group.herokuapp.com" 
+   :domain               => "success-group.herokuapp.com",
    :user_name            => "traveltheglobe4free@gmail.com",
    :password             => "wosgioma123",
    :authentication       => "plain",
